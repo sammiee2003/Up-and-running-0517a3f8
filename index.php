c9c930592fb86eb25819b32e85f9113e260279af
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-04-23 13:35:44 +0200

    Uitwerking van de exercise

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-    $dbh = new PDO (
-        'mysql: host=localhost;port:3306;dbname=netland',
-        'root',
-        ''
-    ) ;
-    echo 'connected to Netland';
-    echo $dbh -> query('select version()') -> fetchcolumn();
+$dbh = new PDO(
+    'mysql: host=localhost;port:3306;dbname=netland',
+    'root',
+    ''
+) ;
+echo 'connected to Netland';
+echo $dbh -> query('select version()') -> fetchcolumn();
 
 ?>
